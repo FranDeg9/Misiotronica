d8ed809c5c58b5c2c0eab202d6d501f0e3b36dbb
FranDeg9 <degiovanni.franco1@gmail.com>
2023-08-08 13:34:14 -0300

    correciones de issue

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Service.css';
+import error404 from './error404.jpg';
+
 
 const Service = () => {
   return (
     <div className="service-container">
       <img
         className="background-image"
-        src="error404.jpg"
+        src={error404}
         alt="Imagen de fondo"
       />
-      {<h1>error404</h1>}
+      {/*{<h1>error404</h1>}*/}
     </div>
   );
 };
