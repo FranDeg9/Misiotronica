bc671abff248e463b3212fe2d856131afc9086d2
FranDeg9 <degiovanni.franco1@gmail.com>
2023-08-10 22:05:35 -0300

    main correction style final

--- src/components/Background.js
+++ src/components/Background.js
@@ -16,7 +16,7 @@ const Background = () => {
         width: '100%',
         height: '100%',
         zIndex: -1,
-        opacity: 0.9,
+        opacity: 1,
       }}
     />
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import "./Home.css";
+import productImage2 from './image/detectordemovimientos.png';
+import productImage3 from './image/estabilizador.png';
+import productImage6 from './image/transformador.png';
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>Bienvenidos a Misiotronica</h1>
-      
       <p className='text'><b>Descubre una amplia gama de productos electrónicos y tecnológicos para tu hogar y oficina.</b></p>
-      
+      <div className="special-offers">
+        <h2>¡Ofertas y Descuentos!</h2>
+        <Carousel showThumbs={false} showStatus={false} infiniteLoop={true}>
+          <div>
+            <div className="offer" >
+              <a href='/venta'>
+              <img className='image-size' src={productImage2} alt="Oferta 1"  />
+              <p>¡Gran oferta en detector de movimientos! Aprovecha el descuento del 20%!!!</p>
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="offer">
+              <a href='/venta'>
+              <img className='image-size' src={productImage3} alt="Oferta 2" />
+              <p>¡No te pierdas la oferta en Estabilizadores! Compra uno y llévate el segundo a mitad de precio!!!.</p>
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="offer">
+            <a href='/venta'>
+              <img className='image-size' src={productImage6} alt="Oferta 2" />
+              <p>¡No te pierdas esta oportunidad y llevate tu transformador en cuotas sin interes!!!.</p>
+            </a>
+            </div>
+          </div>
+          {/* Agregar más ofertas */}
+        </Carousel>
+      </div>
       <div className="social-links">
         <h3>Nuestras Redes Sociales</h3>
         <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
@@ -22,7 +55,7 @@ const Home = () => {
           <FontAwesomeIcon icon={faInstagram} />
         </a>
       </div>
-      
+
       <div className="quick-links">
         <a href="/venta">Productos en Venta</a>
         <a href="/sobre-nosotros">Sobre Nosotros</a>
